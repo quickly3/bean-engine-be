9678f7c5bb7b8c6390ac9254857f2089399c9f99
Hongbin Zhou <>
2024-02-18 10:12:04 +0800

    update

--- shell/weekly_crawl.sh
+++ shell/weekly_crawl.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 export PATH=$PATH:/usr/local/bin   
 
-cd /home/ubuntu/www/ng-blog/scrapy
-nohup python3 -m scrapy crawl elastic_cn >> /home/ubuntu/www/ng-blog/storage/logs/elastic_cn.log 2>&1
-# nohup python3 -m scrapy crawl csdn >> /home/ubuntu/www/ng-blog/storage/logs/csdn.log 2>&1
+cd /home/ubuntu/www/bean-engine-be/scrapy
+nohup python3 -m scrapy crawl elastic_cn >> /home/ubuntu/www/bean-engine-be/output/elastic_cn.log 2>&1
+# nohup python3 -m scrapy crawl csdn >> /home/ubuntu/www/bean-engine-be/output/csdn.log 2>&1
 
 
 cd /home/ubuntu/www/ng-blog
-nohup npm run cli esClear >> /home/ubuntu/www/ng-blog/storage/logs/elastic_cn.log 2>&1
-# nohup sudo php artisan EsClear csdn >> /home/ubuntu/www/ng-blog/storage/logs/csdn.log 2>&1
+nohup npm run cli esClear >> /home/ubuntu/www/bean-engine-be/output/elastic_cn.log 2>&1
+# nohup sudo php artisan EsClear csdn >> /home/ubuntu/www/bean-engine-be/output/csdn.log 2>&1
 
