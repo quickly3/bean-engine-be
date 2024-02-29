4da64f2590e72f8c12ae90f0977d6d4ac7025a8c
Hongbin Zhou <>
2024-02-29 16:32:35 +0800

    update

--- src/controller/feishu.controller.ts
+++ src/controller/feishu.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 
 @Controller('feishu')
 export class FeishuController {
   constructor() {}
 
   @Post('event')
+  @HttpCode(200)
   async event(@Body() payload) {
     console.log('payload', payload);
     return true;
