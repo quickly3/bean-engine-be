9307e064c9c41eb476a3e657723ca32d832691ee
Hongbin Zhou <>
2024-05-06 15:14:33 +0800

    update

--- scrapy/tutorial/spiders/csdn_news.py
+++ scrapy/tutorial/spiders/csdn_news.py
@@ -25,7 +25,8 @@ class AliSpider(scrapy.Spider):
     source = "csdn"
 
     headers= {
-        'Accept': 'application/json, text/plain, */*',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'Accept-Encoding':'gzip, deflate, br, zstd',
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
     }
 
