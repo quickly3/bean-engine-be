d07aba05e20caf20592469276a4c0aa8f698b436
Hongbin Zhou <>
2025-02-26 15:39:19 +0800

    no message

--- py/openai/test.py
+++ py/openai/test.py
@@ -4,26 +4,22 @@ from dotenv import load_dotenv
 from pathlib import Path
 import httpx
 
-
-env_path = Path('../..')/'.env'
+env_path = Path('../..') / '.env'
 load_dotenv(dotenv_path=env_path)
 
 GPT_KEY = os.getenv("GPT_KEY")
 
-client = OpenAI(api_key=GPT_KEY, base_url='https://',     timeout=30000, max_retries=0,
-                             http_client=httpx.Client(
-                                 proxies="http://127.0.0.1:7890",
-                                 transport=httpx.HTTPTransport(local_address="0.0.0.0"),
-                                 verify=False
-                             ))
-
+client = OpenAI(
+    api_key=GPT_KEY,
+    base_url='https://api.openai-proxy.com/v1',
+)
 
 chat_completion = client.chat.completions.create(
-    messages=[
-        {
-            "role": "user",
-            "content": "Say this is a test",
-        }
-    ],
+    messages=[{
+        "role": "user",
+        "content": "Say this is a test",
+    }],
     model="gpt-4o",
-)
+    session_id="chatcmpl-B27kpvOMdLmCAzXhxEnrVIE9Zyg1b")
+
+print(chat_completion)
