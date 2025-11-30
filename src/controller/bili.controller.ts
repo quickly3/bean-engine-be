496a72d453faaebe83deba47b1b9f81334d26f3e
Hongbin Zhou <>
2025-11-30 09:53:16 +0800

    update

--- src/controller/bili.controller.ts
+++ src/controller/bili.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { Prisma } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { convertBigIntToNumberInArray } from 'src/service/bili/bili-util';
 
@@ -12,18 +13,33 @@ export class BiliController {
 
   @Post('getUps')
   async getUps(@Body() payload: any) {
-    const { page, pageSize } = payload;
-    const ups = await this.prisma.biliUps.findMany({
-      skip: (page - 1) * pageSize,
-      take: pageSize,
-      orderBy: { id: 'asc' },
-    });
-    // ups.map((u: any) => {
-    //   u.spaceUrl = BiliUrl.space.replace('{mid}', u.mid.toString());
-    // });
+    const { page, pageSize, keywords } = payload;
+
+    const where: any = {};
+    let whereStr = Prisma.sql``;
+
+    if (keywords && keywords.trim() !== '') {
+      where.uname = { contains: keywords.trim() };
+      whereStr = Prisma.sql`WHERE b.uname LIKE ${'%' + keywords.trim() + '%'}`;
+    }
+
+    const sql = Prisma.sql`
+      SELECT b.*,count(1) "totalVideos",max(ba.created) "lastPublish"
+        FROM "BiliUps" b 
+        LEFT JOIN "BiliArchive" ba on ba.mid = b.mid
+        ${whereStr}
+        GROUP BY b.id
+        ORDER BY b.id asc
+        LIMIT ${pageSize} OFFSET ${(page - 1) * pageSize};
+    `;
+
+    const ups: any[] = await this.prisma.$queryRaw(sql);
+
+    const total = await this.prisma.biliUps.count({ where });
     return {
       ...payload,
       data: convertBigIntToNumberInArray(ups),
+      total,
     };
   }
 
@@ -36,9 +52,13 @@ export class BiliController {
       take: pageSize,
       orderBy: { id: 'asc' },
     });
+
+    const total = await this.prisma.biliArchive.count();
+
     return {
       ...payload,
       data: convertBigIntToNumberInArray(videos),
+      total,
     };
   }
 }
